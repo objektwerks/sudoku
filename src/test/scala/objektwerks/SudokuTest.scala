bd176b891085fac5f56433ed1b4d63790ef512d2
objektwerks <objektwerks@runbox.com>
2025-04-29 20:23:42 -0400

    edited sudoku test

--- src/test/scala/objektwerks/SudokuTest.scala
+++ src/test/scala/objektwerks/SudokuTest.scala
@@ -6,3 +6,4 @@ import org.scalatest.matchers.should.Matchers
 final class SukokuTest extends AnyFunSuite with Matchers:
   test("sudoku")
     "todo".nonEmpty shouldBe true
+    println("*** sudoku test passed")
