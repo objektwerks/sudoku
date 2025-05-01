23293fd9b403e25312ebc3504f4a31defb58c734
objektwerks <objektwerks@runbox.com>
2025-05-01 10:00:45 -0400

    added to string to solver

--- src/main/scala/objektwerks/Solver.scala
+++ src/main/scala/objektwerks/Solver.scala
@@ -2,3 +2,12 @@ package objektwerks
 
 object Solver: 
   type Board = Array[Array[Int]]
+
+  def toString(board: Board): String =
+    board.grouped(3).map: bigGroup =>
+      bigGroup.map: row =>
+        row.grouped(3).map: smallGroup =>
+          smallGroup.mkString(" ", " ", " ")
+        .mkString("|", "|", "|")
+      .mkString("\n")
+    .mkString("+-------+-------+-------+\n", "\n+-------+-------+-------+\n", "\n+-------+-------+-------+")
