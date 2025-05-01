43893e6babd1c03a4c1ee9809f85597b6c074ff4
objektwerks <objektwerks@runbox.com>
2025-05-01 09:45:36 -0400

    refactored digit

--- src/main/scala/objektwerks/Grid.scala
+++ src/main/scala/objektwerks/Grid.scala
@@ -1,6 +1,11 @@
 package objektwerks
 
-final case class Digit()
+opaque type Digit = Int
+
+object Digit:
+  def apply(digit: Int): Digit = digit
+  extension (digit: Digit)
+    def value: Int = digit
 
 final case class Cell(digit: Digit)
 
