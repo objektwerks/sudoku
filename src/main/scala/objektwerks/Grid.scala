1704a6155d5a29eb70338b9d6122646c1fc23281
objektwerks <objektwerks@runbox.com>
2025-05-01 09:53:27 -0400

    removed row, collumn and cell

--- src/main/scala/objektwerks/Grid.scala
+++ src/main/scala/objektwerks/Grid.scala
@@ -6,11 +6,5 @@ object Digit:
   def apply(digit: Int): Digit = digit
   extension (digit: Digit)
     def value: Int = digit
-
-final case class Cell(digit: Digit)
-
-final case class Row(cells: Set[Cell])
-
-final case class Column(cells: Set[Cell])
  
-final case class Grid(rows: Set[Cell], columns: Set[Cell])
+final case class Grid(grid: Array[Array[Digit]])
