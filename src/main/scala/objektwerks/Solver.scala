deb3c3111ee6ca183eb895506b24145903e0126c
objektwerks <objektwerks@runbox.com>
2025-05-01 09:56:13 -0400

    refactored solver

--- src/main/scala/objektwerks/Solver.scala
+++ src/main/scala/objektwerks/Solver.scala
@@ -1,12 +1,11 @@
 package objektwerks
 
-opaque type Digit = Int
+object Solver:
+  opaque type Digit = Int
 
-object Digit:
-  def apply(digit: Int): Digit = digit
-  extension (digit: Digit)
-    def value: Int = digit
- 
-type Grid = Array[Array[Digit]]
-
-final case class Solver(grid: Grid)
+  object Digit:
+    def apply(digit: Int): Digit = digit
+    extension (digit: Digit)
+      def value: Int = digit
+   
+  type Grid = Array[Array[Digit]]
