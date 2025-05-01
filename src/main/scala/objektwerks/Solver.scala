0d70ed8819ea9f859aea5c4c43fafb60268a4a1a
objektwerks <objektwerks@runbox.com>
2025-05-01 10:21:23 -0400

    enhanced solver

--- src/main/scala/objektwerks/Solver.scala
+++ src/main/scala/objektwerks/Solver.scala
@@ -3,6 +3,31 @@ package objektwerks
 object Solver: 
   type Board = Array[Array[Int]]
 
+/* 
+  def solve(): Option[Array[Array[Int]]] = solveHelper(0, 0)
+  private def solveHelper(row: Int, col: Int): Option[Array[Array[Int]]] =
+    if (row == 9) {
+      Some(puzzle)
+    } else if (col == 9) {
+      solveHelper(row + 1, 0)
+    } else if (puzzle(row)(col) != 0) {
+      solveHelper(row, col + 1)
+    } else {
+      val validDigits = getValidDigits(row, col)
+      validDigits.foreach { digit =>
+        puzzle(row)(col) = digit
+        val result = solveHelper(row, col + 1)
+        if (result.isDefined) {
+          return result
+        }
+        puzzle(row)(col) = 0
+      }
+      None
+    }
+
+  private def getValidDigits(row: Int, col: Int): Seq[Int] = // Logic to determine valid digits for the given row and column
+*/
+
   def solve(board: Board,
             x: Int = 0,
             y: Int = 0): Unit =
@@ -12,10 +37,12 @@ object Solver:
     
     else if (board(y)(x) > 0) solve(board, x + 1, y) // need to fill in the next cell (cell to the right)
     
-    else (1 to 9).filter(value => validate(board, x, y, value)).foreach: value =>
-      board(y)(x) = value // fill the sudoku board with the value
-      solve(board, x + 1, y) // try the next cell
-      board(y)(x) = 0 // remove the value
+    else (1 to 9)
+      .filter(value => validate(board, x, y, value))
+      .foreach: value =>
+        board(y)(x) = value // fill the sudoku board with the value
+        solve(board, x + 1, y) // try the next cell
+        board(y)(x) = 0 // remove the value
 
   def validate(board: Board,
                x: Int,
